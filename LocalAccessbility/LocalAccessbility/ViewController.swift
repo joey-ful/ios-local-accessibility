d235e4b14571d5eff2c78653ce464e9b40485d4f
joey-ful <isjoeyful@gmail.com>
2021-10-11 16:04:17 +0900

    text, currency 기호, 날짜포맷, 이미지 지역화

--- LocalAccessbility/LocalAccessbility/ViewController.swift
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -10,13 +10,47 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var dateTimeLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    let touchMe = "Touch Me!"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addBottomLabel()
+        addButton()
         
+        localizeDate()
+        localizeCurrency()
+    }
+    
+    func localizeCurrency() {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = .current
+        numberFormatter.numberStyle = .currency
+        
+        
+        currencyLabel.text = numberFormatter.string(from: 3000.34)
+//        numberFormatter.currencyCode = .
+//        NumberFormatter.localizedString(from: 3000.34 as NSNumber, number: .currency)
+    }
+    
+    func localizeDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .medium
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+
+        let date = Date()
+        dateTimeLabel.text = dateFormatter.string(from: date)
+    }
+    
     private func addBottomLabel() {
         
         let bottomLabel = UILabel()
         bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
         
-        bottomLabel.text = "Touch the button below!"
+        bottomLabel.text = "Touch the button below!".localized()
         
         bottomLabel.font = UIFont.systemFont(ofSize: 20)
         
@@ -25,6 +59,7 @@ class ViewController: UIViewController {
         bottomLabel.textAlignment = .center
         
         view.addSubview(bottomLabel)
+        
     }
     
     private func addButton() {
@@ -33,7 +68,7 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
         
-        button.setTitle("Touch Me!", for: .normal)
+        button.setTitle("Touch Me!".localized(), for: .normal)
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         
@@ -41,13 +76,10 @@ class ViewController: UIViewController {
         
         view.addSubview(button)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addBottomLabel()
-        addButton()
-        
-        dateTimeLabel.text = Date().description
-    }
 }
 
+extension String {
+    func localized(bundle: Bundle = .main, tableName: String = "Main") -> String {
+        return NSLocalizedString(self, tableName: tableName, comment: "")
+    }
+}
